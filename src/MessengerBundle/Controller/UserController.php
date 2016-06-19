2dc486bb3a235db18235d3d9e78bca97562690ac
luninroman <lunin.roman91@gmail.com>
2016-06-19 19:42:29 +0300

    Finished register page

--- src/MessengerBundle/Controller/UserController.php
+++ src/MessengerBundle/Controller/UserController.php
@@ -3,11 +3,19 @@
 namespace MessengerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 class UserController extends Controller
 {
     public function profileAction($id)
     {
-        return $this->render('MessengerBundle:Messenger/User:profile.html.twig', array('id' => $id));
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $user = $em->getRepository('MessengerBundle:User')->find($id);
+        if (! $user) {
+            throw new Exception('User with id '.$id.' was not found!');
+        }
+
+        return $this->render('MessengerBundle:Messenger/User:profile.html.twig', array('user' => $user));
     }
 }
--- src/MessengerBundle/Entity/User.php
+++ src/MessengerBundle/Entity/User.php
@@ -19,4 +19,155 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * @ORM\Column(
+     *     type="string",
+     *     length=25,
+     *     nullable=true
+     * )
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(
+     *     type="string",
+     *     nullable=true
+     * )
+     * 
+     * @var string
+     */
+    private $about;
+
+    /**
+     * @ORM\Column(
+     *     type="string"
+     * )
+     *
+     * @var string
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(
+     *    type="boolean"
+     *)
+     *
+     * @var bool
+     */
+    private $gender;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->gender = true;
+        $this->image = 'user.png';
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get title
+     * 
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Get gender
+     *
+     * @return bool
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * Get about
+     * 
+     * @return string
+     */
+    public function getAbout()
+    {
+        return $this->about;
+    }
+
+    /**
+     * Get image name
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set title
+     * 
+     * @param $title
+     * 
+     * @return User
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        
+        return $this;
+    }
+
+    /**
+     * Set gender
+     *
+     * @param $gender
+     *
+     * @return User
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    /**
+     * Set about
+     * 
+     * @param $about
+     * 
+     * @return User
+     */
+    public function setAbout($about)
+    {
+        $this->about = $about;
+        
+        return $this;
+    }
+
+    /**
+     * Set image name
+     *
+     * @param $image
+     *
+     * @return User
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
 }
