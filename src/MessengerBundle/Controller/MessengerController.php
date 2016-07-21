0d6488856af6be42d2cc889fb15bdbf062c02c0a
luninroman <lunin.roman91@gmail.com>
2016-07-21 10:10:16 +0300

    update changes

--- src/MessengerBundle/Controller/MessengerController.php
+++ src/MessengerBundle/Controller/MessengerController.php
@@ -4,6 +4,7 @@ namespace MessengerBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 class MessengerController extends Controller
 {
